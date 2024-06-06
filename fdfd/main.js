9ef46c2404efcf7fd0bf676b18b0cd6d2f446552
joaovictor307 <163419377+joaovictor307@users.noreply.github.com>
2024-06-06 15:26:27 -0300

    Add files via upload

---
+++ fdfd/main.js
@@ -0,0 +1,7 @@
+let valorproduto=prompt("digite o valor do produto: ")
+let valordesconto=(valorproduto*0.7)
+if (valordesconto>=200) {
+    alert("valor do desconto é de 30% o valor final é de: "+valordesconto)
+}else {
+    alert("esse valor não possui desconto")
+}
